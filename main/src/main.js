170699a69c8e75ed0b4ee441f834a568ef559ec4
ankera <15189120919@163.com>
2023-07-13 14:27:26 +0800

    feat: 发布平台

--- main/src/main.js
+++ main/src/main.js
@@ -1,9 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { subNavList } from './store/sub'
-import { registerApp } from './util'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { subNavList } from "./store/sub";
+import { registerApp } from "./util";
 
-registerApp(subNavList)
+registerApp(subNavList);
 
-createApp(App).use(router()).mount('#micro_web_main_app')
+createApp(App).use(router()).mount("#micro_web_main_app");
+
+// history.go、  history.back、  history.forward 触发 popstate
+window.addEventListener("popstate", function () {
+  console.log("popstate");
+});
+
+const oldPushState = window.history.pushState;
+window.history.pushState = function (...args) {
+  oldPushState.apply(window.history, args);
+  console.log("pushState");
+};
+
+const oldReplaceState = window.history.replaceState;
+window.history.replaceState = function (...args) {
+    oldReplaceState.apply(window.history, args);
+  console.log("replaceState");
+};
